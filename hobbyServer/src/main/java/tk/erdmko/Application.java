144221a0d9bc921e842278bef92103f20cc0dc7a
erdmko <erdmko@gmail.com>
2015-10-05 16:01:23 +0300

    Hello world with static resources

--- hobbyServer/src/main/java/tk/erdmko/Application.java
+++ hobbyServer/src/main/java/tk/erdmko/Application.java
@@ -2,12 +2,18 @@ package tk.erdmko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configuration(SpringApplicationBuilder app) {
+        return app.sources(Application.class);
+    }
     public static void main(String[] args){
-        ApplicationContext context = SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication
+                .run(Application.class, args);
 
     }
 }
--- hobbyServer/src/main/java/tk/erdmko/conrollers/mainPage.java
+++ hobbyServer/src/main/java/tk/erdmko/conrollers/mainPage.java
@@ -5,14 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Created by erdmko on 23/09/15.
- */
 @Controller
-public class mainPage {
+public class MainPage {
+
     @RequestMapping("/")
-    public String index(@RequestParam(value = "name", required = false, defaultValue = "undef") String name, Model model){
+    public String index(
+        @RequestParam(
+            value = "name",
+            required = false,
+            defaultValue = ""
+        )
+        String name,
+        Model model
+    ) {
         model.addAttribute("name", name);
-        return "Hi";
+        return "index";
     }
 }
