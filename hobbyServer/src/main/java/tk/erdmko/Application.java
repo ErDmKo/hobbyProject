745e7bdf7206dca6dbea18a4c7fa5b7d7016ae92
erdmko <erdmko@gmail.com>
2017-04-19 20:46:17 +0300

    Up versions

--- hobbyServer/src/main/java/tk/erdmko/Application.java
+++ hobbyServer/src/main/java/tk/erdmko/Application.java
@@ -1,19 +1,27 @@
 package tk.erdmko;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
 import javax.sql.DataSource;
 
 @SpringBootApplication
@@ -34,6 +42,16 @@ public class Application extends SpringBootServletInitializer {
 
     }
     @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        final LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan(new String[] { "tk.erdmko.users.model" });
+
+        final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        emf.setJpaVendorAdapter(vendorAdapter);
+        return emf;
+    }
+    @Bean
     public CommonsRequestLoggingFilter requestLoggingFilter() {
         CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
         filter.setIncludeQueryString(true);
@@ -48,11 +66,26 @@ public class Application extends SpringBootServletInitializer {
     }
 
     @Bean
-    public SessionFactory sessionFactory() {
-        AnnotationSessionFactoryBean asFactoryBean =
-                new AnnotationSessionFactoryBean();
+    @Primary
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean asFactoryBean =
+                new LocalSessionFactoryBean();
         asFactoryBean.setDataSource(dataSource());
+        asFactoryBean.setPackagesToScan(new String[] { "tk.erdmko.users.model" });
         // additional config
-        return asFactoryBean.getObject();
+        return asFactoryBean;
+    }
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory){
+        HibernateTransactionManager txManager
+                = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
     }
 }
