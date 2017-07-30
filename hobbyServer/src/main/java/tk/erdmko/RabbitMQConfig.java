8389acec618443e973fbe06a7803c191f00ae988
erdmko <erdmko@gmail.com>
2017-07-30 19:31:25 +0300

    docker-compose

--- hobbyServer/src/main/java/tk/erdmko/RabbitMQConfig.java
+++ hobbyServer/src/main/java/tk/erdmko/RabbitMQConfig.java
@@ -1,11 +1,13 @@
 package tk.erdmko;
 
 import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -16,6 +18,9 @@ import tk.erdmko.models.SocketResponseModel;
 
 @Configuration
 public class RabbitMQConfig {
+    final private static String defaultQueueName = "hello";
+    private Queue defaultQueue = new Queue(defaultQueueName);
+    private @Value("${spring.amqp.host}") String host;
 
     @Autowired
     private SimpMessagingTemplate webSocket;
@@ -23,17 +28,18 @@ public class RabbitMQConfig {
     @Bean
     public CachingConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory =
-                new CachingConnectionFactory("localhost");
-
+                new CachingConnectionFactory(host);
         return connectionFactory;
     }
 
     @Bean
     public AmqpAdmin amqpAdmin() {
-        return new RabbitAdmin(connectionFactory());
+        AmqpAdmin out = new RabbitAdmin(connectionFactory());
+        out.declareQueue(defaultQueue);
+        return out;
     }
 
-    @RabbitListener(queues = "hello")
+    @RabbitListener(queues = RabbitMQConfig.defaultQueueName)
     public void processMobileMessages(byte[] message) throws UnsupportedEncodingException {
         webSocket.convertAndSend("/wsOut", new SocketResponseModel(new String(message, "UTF-8")));
     }
